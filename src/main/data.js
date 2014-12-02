c743164253cec19335b56557087975791261d6d0
Mike Cano <mcano808@gmail.com>
2014-12-02 12:14:14 -0700

    Basic game data layout

---
+++ src/main/data.js
@@ -0,0 +1,27 @@
+
+var rule={
+  id:'1', //Uuid
+  shortText:'',
+  longText:'',
+  refPage:''
+};
+
+var rules = {
+  r1:<rule>,
+  r2:<rule>
+}
+
+var scenario = {
+  id:'',
+  name:'',
+  type:'',
+  roundCount:'',
+  specialRules:['r1', 'r2'],
+  
+};
+var game={
+  _gameSystem:'',
+  _ver#:'',
+  _auth:'',
+  scenarios:[<sce
+}
