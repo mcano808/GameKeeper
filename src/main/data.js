a09cbac815f0a74e455268659267072ca46637a7
Mike Cano <mcano808@gmail.com>
2014-12-04 20:54:59 -0700

    completed a sample game system in the data.

--- src/main/data.js
+++ src/main/data.js
@@ -1,27 +1,86 @@
 
-var rule={
-  id:'1', //Uuid
-  shortText:'',
-  longText:'',
-  refPage:''
-};
 
-var rules = {
-  r1:<rule>,
-  r2:<rule>
-}
-
-var scenario = {
-  id:'',
-  name:'',
-  type:'',
-  roundCount:'',
-  specialRules:['r1', 'r2'],
-  
-};
-var game={
-  _gameSystem:'',
-  _ver#:'',
-  _auth:'',
-  scenarios:[<sce
-}
+var data = {Game:{
+    gameSystem:'Test System',
+    ver:'0.1.0',
+    auth:'Mike Cano',
+    scenarios:[{
+        id:1,
+        name:'Scenario 1',
+        type:'',
+        roundCount:6,
+        specialRules:['r1', 'r3', 'r5']
+    },{
+        id:2,
+        name:'Scenario 2',
+        type:'type 2',
+        roundCount:5,
+        specialRules:['r2','r3']
+    },{
+        id:3,
+        name:'Scenario 3',
+        type:'type 1',
+        roundCount:3,
+        specialRules:[]
+    }],
+    rules:{
+        r1:{
+            id:'1',
+            shortText:'rule 1',
+            longText:'This is the long text for rule 1.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'101'
+        }, r2:{
+            id:'2',
+            shortText:'rule 2',
+            longText:'This is the long text for rule 2.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'204'
+        },
+        r3:{
+            id:'3',
+            shortText:'rule 3',
+            longText:'This is the long text for rule 3.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'342'
+        },
+        r4:{
+            id:'4',
+            shortText:'rule 4',
+            longText:'This is the long text for rule 4.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'403'
+        },
+        r5:{
+            id:'5',
+            shortText:'rule 5',
+            longText:'This is the long text for rule 5.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'561'
+        },
+        r6:{
+            id:'6',
+            shortText:'rule 6',
+            longText:'This is the long text for rule 6.  It is very long and takes up lots of space.  This section will explain the rule completly',
+            refPage:'603'
+        },
+        r7:{
+            id:'7',
+          shortText:'rule 7',
+          longText:'This is the long text for rule 7.  It is very long and takes up lots of space.  This section will explain the rule completly',
+          refPage:'710'
+        },
+        r8:{
+            id:'8',
+          shortText:'rule 8',
+          longText:'This is the long text for rule 8.  It is very long and takes up lots of space.  This section will explain the rule completly',
+          refPage:'892'
+        },
+        r9:{
+            id:'9',
+          shortText:'rule 9',
+          longText:'This is the long text for rule 9.  It is very long and takes up lots of space.  This section will explain the rule completly',
+          refPage:'900'
+        },
+        r10:{
+            id:'10',
+          shortText:'rule 10',
+          longText:'This is the long text for rule 10.  It is very long and takes up lots of space.  This section will explain the rule completly',
+          refPage:'999'
+        }
+    }
